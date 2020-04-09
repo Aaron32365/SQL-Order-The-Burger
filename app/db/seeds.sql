ae33309253793a56d836693d59440b7386118e89
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-04-08 20:41:59 -0700

    commited functional app, needs css adjustment

---
+++ app/db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('chicken');
+INSERT INTO burgers (name) VALUES ('plain');
+INSERT INTO burgers (name, ate) VALUES ('steak', true);
+INSERT INTO burgers (name, ate) VALUES ('veggie', true);
